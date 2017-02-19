fec6a4d36ca77c1be30933b36f7500e18a9798bc
shsv382 <shsv382@gmail.com>
2017-02-19 20:59:26 +0400

    Added HTML-form (from Mint-project)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+	
+	def index
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:success]
+			redirect_to root_path
+		else
+			flash.now[:error]
+			render :new
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:username, :email, :password)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create]
+  resources :users, only: [:index, :new, :create]
+  root to: "users#index"
 end
